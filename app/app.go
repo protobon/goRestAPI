6ebf187ca8e4d0d2c265a07850ca2d75e7ac4960
ayrton-hbtn <3282@holbertonschool.com>
2022-12-11 20:46:59 -0300

    init Product & Credit modules

--- app/app.go
+++ app/app.go
@@ -3,12 +3,10 @@ package app
 import (
 	"awesomeProject/app/routes"
 	"awesomeProject/database"
-	"awesomeProject/model"
 	"database/sql"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
-	"strconv"
 )
 
 type App struct {
@@ -35,130 +33,7 @@ func (a *App) Initialize(user string, password string, dbname string) {
 	a.Router = gin.Default()
 
 	var credit = routes.Credit{Router: a.Router, DB: a.DB}
+	var product = routes.Product{Router: a.Router, DB: a.DB}
 	credit.InitializeRoutes(credit.DB)
+	product.InitializeRoutes(product.DB)
 }
-
-func (a *App) getProduct(c *gin.Context) {
-	productId := c.Param("id")
-	id, err := strconv.Atoi(productId)
-	if err != nil {
-		c.JSON(400, map[string]string{"error": "Invalid Product ID"})
-		return
-	}
-
-	p := model.Product{ID: id}
-	if err := p.GetProduct(a.DB); err != nil {
-		switch err {
-		case sql.ErrNoRows:
-			c.JSON(404, map[string]string{"error": "Product not found"})
-		default:
-			c.JSON(500, err.Error())
-		}
-		return
-	}
-
-	c.JSON(200, p)
-}
-
-func (a *App) createProduct(c *gin.Context) {
-	var p model.Product
-	if err := c.BindJSON(&p); err != nil {
-		c.JSON(400, map[string]string{"error": "Invalid request payload"})
-		return
-	}
-
-	if err := p.CreateProduct(a.DB); err != nil {
-		c.JSON(400, map[string]string{"error": "Could not create new Product"})
-		return
-	}
-
-	c.JSON(200, p)
-}
-
-func (a *App) updateProduct(c *gin.Context) {
-	productId := c.Param("id")
-	id, err := strconv.Atoi(productId)
-	if err != nil {
-		c.JSON(400, map[string]string{"error": "Invalid Product id"})
-		return
-	}
-
-	var p model.Product
-	if err := c.BindJSON(&p); err != nil {
-		c.JSON(400, map[string]string{"error": "Invalid request payload"})
-		return
-	}
-
-	p.ID = id
-
-	if err := p.UpdateProduct(a.DB); err != nil {
-		switch err {
-		case sql.ErrNoRows:
-			c.JSON(404, map[string]string{"error": "Product not found"})
-		default:
-			c.JSON(500, err.Error())
-		}
-		return
-	}
-
-	c.JSON(200, p)
-}
-
-func (a *App) deleteProduct(c *gin.Context) {
-	productId := c.Param("id")
-	id, err := strconv.Atoi(productId)
-	if err != nil {
-		c.JSON(400, map[string]string{"error": "Invalid Product id"})
-		return
-	}
-
-	p := model.Product{ID: id}
-	if err := p.DeleteProduct(a.DB); err != nil {
-		switch err {
-		case sql.ErrNoRows:
-			c.JSON(404, map[string]string{"error": "Product not found"})
-		default:
-			c.JSON(500, err.Error())
-		}
-		return
-	}
-
-	c.JSON(200, p)
-}
-
-func (a *App) getProducts(c *gin.Context) {
-	start, _ := strconv.Atoi(c.Query("start"))
-	count, _ := strconv.Atoi(c.Query("count"))
-
-	if count > 10 || count < 1 {
-		count = 10
-	}
-	if start < 0 {
-		start = 0
-	}
-
-	products, err := model.GetProducts(a.DB, start, count)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	c.JSON(200, products)
-}
-
-//func (a *App) initializeRoutes() {
-//	a.Router.GET("/products", func(c *gin.Context) {
-//		a.getProducts(c)
-//	})
-//	a.Router.POST("/product", func(c *gin.Context) {
-//		a.createProduct(c)
-//	})
-//	a.Router.GET("/product/:id", func(c *gin.Context) {
-//		a.getProduct(c)
-//	})
-//	a.Router.PUT("/product/:id", func(c *gin.Context) {
-//		a.updateProduct(c)
-//	})
-//	a.Router.DELETE("/product/:id", func(c *gin.Context) {
-//		a.deleteProduct(c)
-//	})
-//}
