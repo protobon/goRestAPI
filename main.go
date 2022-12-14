6014b2ef293cd60b496b07bd79bda662b8d9f42d
ayrton-hbtn <3282@holbertonschool.com>
2022-12-14 19:24:05 -0300

    New: scheduler

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"awesomeProject/app"
-	"fmt"
 	"os"
 )
 
 func main() {
 	a := app.App{}
-
-	fmt.Println("user startup: ", os.Getenv("APP_DB_USERNAME"))
-	fmt.Println("password startup: ", os.Getenv("APP_DB_PASSWORD"))
-	fmt.Println("dbname startup: ", os.Getenv("APP_DB_NAME"))
 
 	a.Initialize(
 		os.Getenv("APP_DB_USERNAME"),
